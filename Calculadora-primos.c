821b2e4920d516f22490e4dea9d07a2bf8ee549a
Doodles3 <eduardomoraes84@yahoo.com.br>
2020-09-13 00:05:30 -0300

    Utilizar stdbool em vez de #define
    Reduz a quantidade de código, e permite que o compilador possivelmente armazene as bool de forma mais eficiente do que unsigned char, no geral é uma forma mais limpa

--- Calculadora-primos.c
+++ Calculadora-primos.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>
-
-#ifndef __cplusplus
-typedef unsigned char bool;
-static const bool false = 0;
-static const bool true = 1;
-#endif
+#include <stdbool.h>
 
 bool ePrimo(unsigned int numero);
 bool numerosPrimos[sizeof(unsigned int)];
@@ -14,11 +9,13 @@ int main(int argc)
 {
     wprintf(L"Memoria alocada!\n");
 
+    // Ao receber argumentos o programa inicia no modo live
     if (argc > 1)
     {
         wprintf(L"Iniciado no modo de debug ao vivo!\n");
     }
 
+    // Alterar modo de output de stdout para suportar UTF-8
     _setmode(_fileno(stdout), 0x00040000);
     wprintf(L"Modo de texto definido para UTF-8!\n");
 
@@ -26,6 +23,9 @@ int main(int argc)
     unsigned int numero;
     scanf("%u", &numero);
 
+    //
+    // MODO LIVE: Os números são printados ao serem calculados, e não são salvos em um arquivo
+    //
     if (argc > 1)
     {
         for (unsigned int i = 0; i <= numero; i++)
@@ -37,13 +37,18 @@ int main(int argc)
         }
     }
 
+    //
+    // MODO PADRÃO: Os números são printados após todos serem calculados, e são salvos em um arquivo
+    //
     if (argc <= 1)
     {
+        // Calcular todos os números primos até o numero
         for (unsigned int i = 0; i <= numero; i++)
         {
             numerosPrimos[i] = ePrimo(i);
         }
 
+        // Abrir arquivo para salvar os números calculados
         FILE *fptr = _wfopen(L"números_primos.txt", L"w,ccs=UTF-8");
         if (!fptr)
         {
@@ -51,6 +56,7 @@ int main(int argc)
             return 1;
         }
 
+        // Printar e salvar todos os números primos
         fwprintf(fptr, L"Os seguintes números são primos:\n");
         wprintf(L"Os seguintes números são primos:\n");
         for (unsigned int i = 0; i < numero; i++)
@@ -61,6 +67,8 @@ int main(int argc)
                 fwprintf(fptr, L"%u, ", i);
             }
         }
+
+        // Salvar arquivo com os números primos
         fclose(fptr);
     }
 
@@ -69,6 +77,7 @@ int main(int argc)
     return 0;
 }
 
+// Retorna true caso numero seja primo, false caso não seja
 bool ePrimo(unsigned int numero)
 {
     for (unsigned int i = 2; i < numero; i++)
